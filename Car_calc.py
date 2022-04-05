29b1a5ec9a2f1acee54624c6ac03a3a14476d0b4
Антоха <pupika@MacBook-Pro-Antoha.local>
2022-04-05 20:00:22 +0300

    Add Output message

--- Car_calc.py
+++ Car_calc.py
@@ -6,3 +6,6 @@ car = input( "Машина" )
 year = int(input("Год выпуска"))
 hp = int(input( "Лошадиные силы" ))
 own = int(input("Срок владения месяцев"))
+#Output
+print("Привет!", name, surname)
+print("Регион", region, "." , "Машина", car, "," , "год выпуска", year, "мощность" , hp, "лошадиных сил")
