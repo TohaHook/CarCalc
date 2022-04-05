d92861fc0f08f18337d70072b99a39475355bae1
Антоха <pupika@MacBook-Pro-Antoha.local>
2022-04-05 19:59:36 +0300

    Add Input

--- Car_calc.py
+++ Car_calc.py
@@ -1,1 +1,8 @@
-
+#Input
+name = input( "Имя" )
+surname = input( "Фамилия" )
+region = input( "Регион" )
+car = input( "Машина" )
+year = int(input("Год выпуска"))
+hp = int(input( "Лошадиные силы" ))
+own = int(input("Срок владения месяцев"))
